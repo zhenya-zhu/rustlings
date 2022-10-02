8f540b151749d2e853cc675ba13a106f0516592e
Zhenya Zhu <zzyagg@gmail.com>
2022-10-02 22:51:38 +0800

    to quiz2

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -3,19 +3,15 @@
 // 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -24,7 +20,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        self::delicious_snacks::fruits::PEAR,
+        self::delicious_snacks::veggies::CUCUMBER
     );
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,15 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count:i32) -> i32 {
+    if apple_count <= 40{
+        apple_count * 2
+    }else {
+        apple_count
+    }
+}
 
 // Don't modify this function!
 #[test]
